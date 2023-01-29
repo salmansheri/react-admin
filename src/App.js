93891e70b613d44ebc123c20ab01ed5e163f7b76
salmansheri <sheriffsalman00@gmail.com>
2023-01-29 23:22:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,311 @@
+import React from 'react';
+import { Topbar } from './components'; 
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Topbar />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       test<br />
+       
+
+
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Topbar.css";
+import { Notifications, Language, Settings } from "@mui/icons-material";
+import myphoto from '/media/salman/main/projects/react-admin/project/client/src/assets/images/my_photo.jpg'; 
+
+function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Salman dashboard</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <Notifications />
+            <span className="topbarIconBadge">2</span>
+
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topbarIconBadge">2</span>
+            
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+           
+            
+          </div>
+          <img className="topbarAvatar " src={myphoto} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
